ed7ba105ae240715bc2f1645122e20eb244c8b5b
SeungHyunH <vkflek1232@naver.com>
2022-06-08 19:27:55 +0900

    axios import 삭제

--- grisetutor/src/Pages/TutorMainPage/List/ConsultItem.jsx
+++ grisetutor/src/Pages/TutorMainPage/List/ConsultItem.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components';
 import { useNavigate } from "react-router-dom";
-import axios from 'axios';
+
 const ConsultingItem = (props) => {
   const navigate = useNavigate();
   const onClickShowConsultBtn = () =>{
